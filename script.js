bea50288223f4bfce70eda6b50a4f0c236d47272
RomanKaptarUA <romankaptar82@gmail.com>
2024-08-09 17:14:07 +0300

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+// element.querySelector(selector);
+// const item = document.querySelector('.list-item');
+// const listItems = document.querySelectorAll('.list-item');
+// console.log(listItems);
+
+const listWidth = 
+document.querySelector('#menu');
+listWidth.style.textTransform = 'uppercase';
+listWidth.style.fontSize = '26px';
+console.log(listWidth);
