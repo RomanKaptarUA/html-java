94ae32c40d69d0a6a5947f7e5647e3d1583ca2a5
RomanKaptarUA <romankaptar82@gmail.com>
2024-08-14 17:09:11 +0300

    .

--- script.js
+++ script.js
@@ -3,8 +3,27 @@
 // const listItems = document.querySelectorAll('.list-item');
 // console.log(listItems);
 
-const listWidth = 
-document.querySelector('#menu');
-listWidth.style.textTransform = 'uppercase';
-listWidth.style.fontSize = '26px';
-console.log(listWidth);
+// const listWidth = 
+// document.querySelector('#menu');
+// listWidth.style.textTransform = 'uppercase';
+// listWidth.style.fontSize = '26px';
+// console.log(listWidth);
+
+// document.parentNode('..');
+// document.childNodes('..');
+
+// document.children('..');
+
+// document.firstChild('..');
+// document.lastChild('..');
+
+// document.firstElementChild('..');
+// document.lastElementChild('..');
+
+// document.previousSibling('..');
+// document.nextSibling('..');
+
+const button = document.querySelector('.btn');
+button.style.backgroundColor = 'orange';
+button.style.color = 'white';
+console.log(button.style);
